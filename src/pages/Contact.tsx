58039fac5babd7f1cda5f40794e981c299d9edd0
erricadg <was_erica@yahoo.com>
2022-07-07 12:30:24 -0700

    added formspree

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,51 +1,70 @@
-import { Box } from "@mui/material";
-import React, { useState } from "react";
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
+// Styles
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../styles/Styles";
+
+// Material Icons
+import Container from "@mui/material/Container";
 import DrawerAppBar from "../Nav";
+import { Box } from "@mui/system";
+import { Typography } from '@mui/material';
 
-const Contact = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e : any) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
+function Contact() {
+  const [state, handleSubmit] = useForm("mayvgjgz");
+  if (state.succeeded) {
+      return  (
+        <ThemeProvider theme={theme}>
+    <Container  maxWidth="lg">
+      <Box mt={20}>
+      <Typography variant="h4" component="h2">
+        Thank you!
+      </Typography>
+      </Box>
+      </Container>
+      <DrawerAppBar />
+      </ThemeProvider>
+      );
+  }
   return (
-    <>
-    <DrawerAppBar />  
-    <Box m={10}>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
+    <ThemeProvider theme={theme}>
+    <Container  maxWidth="lg">
+      <DrawerAppBar />
+       <Box mt={10}>
+       <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
     </form>
-    </Box>
-    </>
+   </Box>
+    </Container>
+  </ThemeProvider>
+     
   );
-};
-
+}
 export default Contact;

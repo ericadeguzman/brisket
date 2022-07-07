086e2ace1e03eabd5ae1c88f5fb8bca4a9c7f9f1
erricadg <was_erica@yahoo.com>
2022-07-06 22:36:20 -0700

    new javascript additons

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,49 @@ import Container from "@mui/material/Container";
 import DrawerAppBar from "./Nav";
 import { Box } from "@mui/system";
 
-import brisket_landscape from "./img/brisket_landscape.jpg";
+// import brisket_landscape from "./img/brisket_landscape.jpg";
+
+function coinToss () {
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  brisket: 'img/brisket_landscape.jpg',
+  wrapped: 'img/brisket_wrapped.jpg'
+};
+
+const img = <img style={{width:"100%",}} src={pics[coinToss() === 'heads' ? 'brisket' : 'wrapped']} />;
+
+const judgmental = Math.random() < 0.5;
+const favoriteFoods = (
+  <div>
+   <Typography variant="h2" component="h2"> My Favorite Food</Typography>
+    <ul>
+      <li>Brisket</li>
+      <li>Bacon</li>
+      { !judgmental && <li>Pork Buttts and I can not lie</li> }
+      <li>BBQ sauce</li>
+    </ul>
+  </div>
+);
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Container  maxWidth="lg">
         <DrawerAppBar />
-       <Box mt={10}>
+         <Box mt={10}>
        <Typography sx={{textAlign: "center", color: '#8A0108', fontSize: { xs: 44 , sm: 44, md: 44, lg: 44, }, lineHeight: .2,}} variant="h1" component="h1">
           Eat more </Typography>
        <Typography sx={{textAlign: "center", color: '#8A0108', fontSize: { xs: 84 , sm: 84, md: 84, lg: 144, },}} variant="h1" component="h1">
           Brisket</Typography>
        </Box>
        <Box my={4} display='flex' flexDirection='row' justifyContent='center'>
-        <img style={{width:'100%',}}  src={brisket_landscape} />
+        {img}
        </Box>
+       {/* <Box>
+       {favoriteFoods}
+       </Box> */}
       </Container>
     </ThemeProvider>
   );

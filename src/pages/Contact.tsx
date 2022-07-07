6f5bfab02d4530fe9c33fab39d838efb7de4b79a
erricadg <was_erica@yahoo.com>
2022-07-06 23:15:40 -0700

    contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,28 +1,51 @@
-import React from "react";
-
-// Styles
-import { ThemeProvider } from "@mui/material/styles";
-import { makeStyles, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
 import DrawerAppBar from "../Nav";
 
-// Components
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-
-import Grid from "@mui/material/Grid";
-import theme from "../styles/Styles";
-
-function Contact() {
+const Contact = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e : any) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth="lg">
-        <DrawerAppBar />
-        <Box my={4}>
-          <Typography variant="h1">Contact</Typography>
-        </Box>
-      </Container>
-    </ThemeProvider>
+    <>
+    <DrawerAppBar />  
+    <Box m={10}>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" required />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
+    </Box>
+    </>
   );
-}
+};
 
 export default Contact;

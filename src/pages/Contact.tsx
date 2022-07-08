4f21f57ac6ce544e18b2f8655f5a1ebc31188593
erricadg <was_erica@yahoo.com>
2022-07-07 19:24:35 -0700

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 // Make sure to run npm install @formspree/react
 // For more help visit https://formspr.ee/react-help
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 // Styles
 import { ThemeProvider } from "@mui/material/styles";
@@ -11,60 +11,56 @@ import theme from "../styles/Styles";
 import Container from "@mui/material/Container";
 import DrawerAppBar from "../Nav";
 import { Box } from "@mui/system";
-import { Typography } from '@mui/material';
+import { Button, TextField, Typography } from "@mui/material";
 
 function Contact() {
   const [state, handleSubmit] = useForm("mayvgjgz");
   if (state.succeeded) {
-      return  (
-        <ThemeProvider theme={theme}>
-    <Container  maxWidth="lg">
-      <Box mt={20}>
-      <Typography variant="h4" component="h2">
-        Thank you!
-      </Typography>
-      </Box>
-      </Container>
-      <DrawerAppBar />
+    return (
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="lg">
+          <Box mt={20}>
+            <Typography variant="h4" component="h2">
+              Thank you!
+            </Typography>
+          </Box>
+        </Container>
+        <DrawerAppBar />
       </ThemeProvider>
-      );
+    );
   }
   return (
     <ThemeProvider theme={theme}>
-    <Container  maxWidth="lg">
-      <DrawerAppBar />
-       <Box mt={10}>
-       <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-   </Box>
-    </Container>
-  </ThemeProvider>
-     
+      <Container maxWidth="lg">
+        <DrawerAppBar />
+        <Box mt={10}  display="flex" flexDirection='column'>
+          <form onSubmit={handleSubmit}>
+           <Box my={2}>
+           {/* <label htmlFor="email">Email Address</label> */}
+            {/* <input id="email" type="email" name="email" /> */}
+            <TextField id="email" type="email" label="Email Address" variant="outlined" />
+
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+           </Box>
+           <Box my={2}>
+           <TextField id="message" type="message" label="Message" multiline maxRows={4} />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+              </Box>
+              <Box my={2}>
+              <Button type="submit" disabled={state.submitting} variant="contained">Submit</Button>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 export default Contact;
